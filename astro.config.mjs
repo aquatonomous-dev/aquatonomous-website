b271d6a306fc873bb15dec9ceaab7e18603d3876
theOneAndOnlyOne <joshgonzales9891@gmail.com>
2024-12-24 15:45:06 -0500

    added search (still broken)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 import react from '@astrojs/react';
+import pagefind from "astro-pagefind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [tailwind(), mdx(), sitemap(), icon(), react()],
+  integrations: [tailwind(), mdx(), sitemap(), icon(), react(), pagefind()],
 });
